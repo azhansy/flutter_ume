956e7686b6475016d37b5de213ceca38bd327d85
dashu <bigtree2018102@gmail.com>
2021-09-09 18:28:39 +0700

    统一引入plugin；

--- lib/flutter_ume.dart
+++ lib/flutter_ume.dart
@@ -7,3 +7,9 @@ export 'core/ui/global.dart';
 export 'service/inspector/inspector_overlay.dart';
 export 'service/vm_service/service_mixin.dart';
 export 'service/vm_service/service_wrapper.dart';
+
+export 'package:flutter_ume_kit_console/flutter_ume_kit_console.dart'; // debugPrint 插件包
+export 'package:flutter_ume_kit_device/flutter_ume_kit_device.dart'; // 设备信息插件包
+export 'package:flutter_ume_kit_perf/flutter_ume_kit_perf.dart'; // 性能插件包
+export 'package:flutter_ume_kit_show_code/flutter_ume_kit_show_code.dart'; // 代码查看插件包
+export 'package:flutter_ume_kit_ui/flutter_ume_kit_ui.dart';
